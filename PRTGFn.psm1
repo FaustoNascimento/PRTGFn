6fe4c7a0a9e4f3fb7e7e69a3dd113172c3a19d95
Nascimento <NascimentoF@duerkopp.local>
2016-07-25 16:01:27 +0200

    Changed pretty much everything...

--- PRTGFn.psm1
+++ PRTGFn.psm1
@@ -133,20 +133,40 @@ function Get-PRTGServerSettings
 
 function Get-PRTGSensor
 {
-    [CmdletBinding(DefaultParameterSetName = 'All')]
+    [CmdletBinding()]
     param
     (
-        [Parameter(ParameterSetName = 'StartAndCount')]
+        [Parameter()]
         [int]
         $Count,
 
-        [Parameter(ParameterSetName = 'StartAndCount')]
+        [Parameter()]
         [int]
         $Start,
 
-        [Parameter(ParameterSetName = 'Id')]
+        [Parameter()]
         [int]
         $Id,
+
+        [Parameter()]
+        [string]
+        $Name,
+
+        [Parameter()]
+        [string]
+        $Tag,
+
+        [Parameter()]
+        [string]
+        $Device,
+
+        [Parameter()]
+        [string]
+        $Group,
+
+        [Parameter()]
+        [string]
+        $ParentId,
 
         [Parameter()]
         [ValidateSet('objid', 'type', 'name', 'tags', 'active', 'downtime', 'downtimetime', 'downtimesince', 'uptime', 'uptimetime', 'uptimesince', 'knowntime', 'cumsince', 'sensor', 'interval', 'lastcheck', 'lastup', 'lastdown', 'device', 'group', 'probe', 'grpdev', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'lastvalue', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'favorite', 'schedule', 'period', 'minigraph', 'comments', 'basetype', 'baselink', 'parentid')]
@@ -160,9 +180,52 @@ function Get-PRTGSensor
             $PSBoundParameters.Add('Columns', @('objid', 'type', 'name', 'tags', 'active', 'downtime', 'downtimetime', 'downtimesince', 'uptime', 'uptimetime', 'uptimesince', 'knowntime', 'cumsince', 'sensor', 'interval', 'lastcheck', 'lastup', 'lastdown', 'device', 'group', 'probe', 'grpdev', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'lastvalue', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'favorite', 'schedule', 'period', 'minigraph', 'comments', 'basetype', 'baselink', 'parentid'))
         }
 
+        $otherParameters = @()
+
+        if ($Id)
+        {
+            $otherParameters += "filter_objid=$Id"
+            [void] $PSBoundParameters.Remove('id')
+        }
+        
+        if ($Name)
+        {
+            $otherParameters += "filter_name=$Name"
+            [void] $PSBoundParameters.Remove('name')
+        }
+
+        if ($Tag)
+        {
+            $otherParameters += "filter_tags=$Tag"
+            [void] $PSBoundParameters.Remove('tag')
+        }
+
+        if ($Device)
+        {
+            $otherParameters += "filter_device=$Device"
+            [void] $PSBoundParameters.Remove('device')
+        }
+
+        if ($Group)
+        {
+            $otherParameters += "filter_group=$Group"
+            [void] $PSBoundParameters.Remove('group')
+        }
+
+        if ($ParentId)
+        {
+            $otherParameters += "filter_parentid=$ParentId"
+            [void] $PSBoundParameters.Remove('parentid')
+        }
+
+        if ($otherParameters.Count -gt 0)
+        {
+            $PSBoundParameters.Add('OtherParameters', $otherParameters)
+        }
+
         $Sensors = ([xml] (Invoke-PRTGCommand -Content sensors @PSBoundParameters).Content).Sensors
         
-        if ($Sensors.Item.Count -gt 0)
+        if ($Sensors.TotalCount -gt 0)
         {
             $Sensors.Item
         }
@@ -171,20 +234,40 @@ function Get-PRTGSensor
 
 function Get-PRTGDevice
 {
-    [CmdletBinding(DefaultParameterSetName = 'All')]
+    [CmdletBinding()]
     param
     (
-        [Parameter(ParameterSetName = 'StartAndCount')]
+        [Parameter()]
         [int]
         $Count,
 
-        [Parameter(ParameterSetName = 'StartAndCount')]
+        [Parameter()]
         [int]
         $Start,
 
-        [Parameter(ParameterSetName = 'Id')]
+        [Parameter()]
         [int]
         $Id,
+
+        [Parameter()]
+        [string]
+        $Name,
+
+        [Parameter()]
+        [string]
+        $Tag,
+
+        [Parameter()]
+        [string]
+        $Host,
+
+        [Parameter()]
+        [string]
+        $Group,
+
+	    [Parameter()]
+        [string]
+        $ParentId,
 
         [Parameter()]
         [ValidateSet('objid', 'type', 'name', 'tags', 'active', 'device', 'group', 'probe', 'grpdev', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'favorite', 'schedule', 'deviceicon', 'comments', 'host', 'basetype', 'baselink', 'icon', 'parentid', 'location')]
@@ -198,16 +281,132 @@ function Get-PRTGDevice
             $PSBoundParameters.Add('Columns', @('objid', 'type', 'name', 'tags', 'active', 'device', 'group', 'probe', 'grpdev', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'favorite', 'schedule', 'deviceicon', 'comments', 'host', 'basetype', 'baselink', 'icon', 'parentid', 'location'))
         }
 
+        $otherParameters = @()
+
+        if ($Id)
+        {
+            $otherParameters += "filter_objid=$Id"
+            [void] $PSBoundParameters.Remove('id')
+        }
+        
+        if ($Name)
+        {
+            $otherParameters += "filter_name=$Name"
+            [void] $PSBoundParameters.Remove('name')
+        }
+
+        if ($Tag)
+        {
+            $otherParameters += "filter_tags=$Tag"
+            [void] $PSBoundParameters.Remove('tag')
+        }
+
+        if ($Host)
+        {
+            $otherParameters += "filter_device=$Host"
+            [void] $PSBoundParameters.Remove('host')
+        }
+
+        if ($Group)
+        {
+            $otherParameters += "filter_group=$Group"
+            [void] $PSBoundParameters.Remove('group')
+        }
+
+	    if ($ParentId)
+        {
+            $otherParameters += "filter_parentid=$ParentId"
+            [void] $PSBoundParameters.Remove('parentid')
+        }
+
+        if ($otherParameters.Count -gt 0)
+        {
+            $PSBoundParameters.Add('OtherParameters', $otherParameters)
+        }
+
         $Devices = ([xml] (Invoke-PRTGCommand -Content devices @PSBoundParameters).Content).Devices
 
-        if ($Devices.Item.Count -gt 0)
+        if ($Devices.TotalCount -gt 0)
         {
             $Devices.Item
         }
     }
 }
 
-function Set-PRTGObject
+function Get-PRTGGroup
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [int]
+        $Count,
+
+        [Parameter()]
+        [int]
+        $Start,
+
+        [Parameter()]
+        [int]
+        $Id,
+
+        [Parameter()]
+        [string]
+        $Name,
+
+        [Parameter()]
+        [string]
+        $Tag,
+
+	    [Parameter()]
+        [string]
+        $ParentId,
+
+        [Parameter()]
+        [ValidateSet('objid', 'type', 'name', 'tags', 'active', 'group', 'probe', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'schedule', 'comments', 'condition', 'basetype', 'baselink', 'parentid', 'location', 'fold', 'groupnum,')]
+        [string[]]
+        $Columns
+    )
+    Process
+    {
+        if (!$PSBoundParameters.ContainsKey('Columns'))
+        {
+            $PSBoundParameters.Add('Columns', @('objid', 'type', 'name', 'tags', 'active', 'group', 'probe', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'schedule', 'comments', 'condition', 'basetype', 'baselink', 'parentid', 'location', 'fold', 'groupnum,'))
+        }
+
+        $otherParameters = @()
+        
+        if ($Name)
+        {
+            $otherParameters += "filter_name=$Name"
+            [void] $PSBoundParameters.Remove('name')
+        }
+
+        if ($Tag)
+        {
+            $otherParameters += "filter_tags=$Tag"
+            [void] $PSBoundParameters.Remove('tag')
+        }
+
+	    if ($ParentId)
+        {
+            $otherParameters += "filter_parentid=$ParentId"
+            [void] $PSBoundParameters.Remove('parentid')
+        }
+
+        $otherParameters += "filter_type=group"
+        $PSBoundParameters.Add('OtherParameters', $otherParameters)
+        
+        $Groups = ([xml] (Invoke-PRTGCommand -Content groups @PSBoundParameters).Content).Groups
+
+        if ($Groups.TotalCount -gt 0)
+        {
+            $Groups.Item
+        }
+    }
+}
+
+function Set-PRTGObjectProperty
 {
     [CmdletBinding()]
     param
@@ -227,45 +426,28 @@ function Set-PRTGObject
     )
     Process
     {
-        Invoke-PRTGCommand -CommandPath api/setobjectproperty.htm -Id $Id -OtherParameters "name=$PropertyName&value=$Value"
+        [void] (Invoke-PRTGCommand -CommandPath api/setobjectproperty.htm -Id $Id -OtherParameters "name=$PropertyName&value=$Value")
     }
 }
 
-function Get-PRTGGroup
+function Get-PRTGObjectProperty
 {
     [CmdletBinding()]
     param
     (
-        [Parameter(ParameterSetName = 'StartAndCount')]
-        [int]
-        $Count,
-
-        [Parameter(ParameterSetName = 'StartAndCount')]
-        [int]
-        $Start,
-
-        [Parameter(ParameterSetName = 'Id')]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias('objid')]
         [int]
         $Id,
 
-        [Parameter()]
-        [ValidateSet('objid', 'type', 'name', 'tags', 'active', 'group', 'probe', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'schedule', 'comments', 'condition', 'basetype', 'baselink', 'parentid', 'location', 'fold', 'groupnum,')]
-        [string[]]
-        $Columns
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $PropertyName
     )
+
     Process
     {
-        if (!$PSBoundParameters.ContainsKey('Columns'))
-        {
-            $PSBoundParameters.Add('Columns', @('objid', 'type', 'name', 'tags', 'active', 'group', 'probe', 'notifiesx', 'intervalx', 'access', 'dependency', 'probegroupdevice', 'status', 'message', 'priority', 'upsens', 'downsens', 'downacksens', 'partialdownsens', 'warnsens', 'pausedsens', 'unusualsens', 'undefinedsens', 'totalsens', 'schedule', 'comments', 'condition', 'basetype', 'baselink', 'parentid', 'location', 'fold', 'groupnum,'))
-        }
-
-        $Groups = ([xml] (Invoke-PRTGCommand -Content group @PSBoundParameters).Content).Groups
-
-        if ($Groups.Item.Count -gt 0)
-        {
-            $Groups.Item
-        }
+        ([xml] (Invoke-PRTGCommand -CommandPath api/getobjectproperty.htm -Id $Id -OtherParameters "name=$PropertyName").Content).PRTG.Result
     }
 }
 
@@ -282,7 +464,7 @@ function Suspend-PRTGObject
 
     Process
     {
-        [null] (Invoke-PRTGCommand -CommandPath "api/pause.htm" -Id $Id -OtherParameters action=0)
+        [void] (Invoke-PRTGCommand -CommandPath "api/pause.htm" -Id $Id -OtherParameters action=0)
     }
 }
 
@@ -299,7 +481,19 @@ function Resume-PRTGObject
 
     Process
     {
-        [null] (Invoke-PRTGCommand -CommandPath "api/pause.htm" -Id $Id -OtherParameters action=1)
+        [void] (Invoke-PRTGCommand -CommandPath "api/pause.htm" -Id $Id -OtherParameters action=1)
+
+        if (Get-PRTGDevice -Id $Id -Columns objid) # if this object is a device already, check for sensors under it to enable
+        {
+            Get-PRTGSensor -ParentId $Id -Columns objid, message | Where-Object message_raw -eq 'Paused by parent' | Resume-PRTGObject
+        }
+        else # otherwise, device might still be a group or a sensor. If it's a group, we cycle through all child objects
+        {
+            foreach ($device in (Get-PRTGDevice -ParentId $Id -Columns objid))
+            {
+                Get-PRTGSensor -ParentId $device.objid -Columns objid, message | Where-Object message_raw -eq 'Paused by parent' | Resume-PRTGObject
+            }
+        }
     }
 }
 
@@ -326,6 +520,149 @@ function Get-PRTGPasswordHash
     }
 }
 
+function Copy-PRTGObject
+{
+    [CmdletBinding(DefaultParameterSetName = 'Device')]
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Device')]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Sensor')]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Group')]
+        [Alias('objid')]
+        [int]
+        $DeviceToCloneId,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Device')]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Sensor')]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Group')]
+        [string]
+        $DisplayName,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Device')]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Sensor')]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Group')]
+        [string]
+        $ParentGroupId,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Device')]
+        [string]
+        $Hostname,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Device')]
+        [switch]
+        $Device,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Sensor')]
+        [switch]
+        $Sensor,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'Group')]
+        [switch]
+        $Group
+    )
+
+    Process
+    {
+        $otherParameters = "name=$DisplayName", "targetid=$ParentGroupId"
+
+        if ($Hostname)
+        {
+            $otherParameters += "host=$Hostname"
+        } 
+        
+        try 
+        {
+            $result = Invoke-PRTGCommand -CommandPath api/duplicateobject.htm -Id $DeviceToCloneId -OtherParameters $otherParameters
+            
+            # The result is the ID of the new object
+            ($result.BaseResponse.responseuri.ToString() -split "ID=")[1].Split('&')[0]
+        }
+        catch 
+        {
+            $PSCmdlets.ThrowTerminatingError((New-ErrorRecord $_.Exception))
+        }
+    }
+}
+
+function New-PRTGSNMPTrafficSensor
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $Interface,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        $ParentId,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $Name = $Interface,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $Comments = $Interface,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [Switch]
+        $Is64Bit = $false,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [int64]
+        $LineSpeed = 0,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]
+        $Tags = @('PRTGFn v1.0', 'snmptrafficsensor', 'bandwidthsensor'),
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateRange(1,5)]
+        [int]
+        $Priority = 3,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $NameChannelIn = 'Traffic In',
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $NameChannelOut = 'Traffic Out',
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $NameChannelTotal = 'Traffic Total',
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]
+        [ValidateSet('errors', 'discards', 'unicast', 'nonunicast', 'multicast', 'broadcast', 'unknown')]
+        $TrafficMode
+    )
+    Process
+    {
+        $otherParameters = @()
+
+        $otherParameters += "interfacenumber__check=$Interface|$Name|||$Comments|$([int][bool]$Is64Bit)||$LineSpeed"
+        $otherParameters += "id=$ParentId"
+        $otherParameters += "priority_=$Priority"
+        $otherParameters += "tags_=$($Tags -join ' ')"
+        $otherParameters += "namein_=$NameChannelIn"
+        $otherParameters += "nameout_=$NameChannelOut"
+        $otherParameters += "namesum_=$NameChannelTotal"
+        $otherParameters += "sensortype=snmptraffic"
+        $otherParameters += "interfacenumber_=1"
+
+        foreach ($mode in $TrafficMode)
+        {
+            $otherParameters += "trafficmode_=$mode"
+        }
+
+        $result = (Invoke-PRTGCommand -CommandPath addsensor5.htm -OtherParameters $otherParameters).Content
+
+        [regex]::Match($result, "<title>.*</title>", "IgnoreCase").Value -notmatch "System Error"
+    }
+}
+
 function Invoke-PRTGCommand
 {
     [CmdletBinding(DefaultParameterSetName = 'All')]
@@ -349,7 +686,7 @@ function Invoke-PRTGCommand
         $CommandPath = 'api/table.xml',
 
         [Parameter()]
-        [ValidateSet('sensortree', 'devices', 'sensors', 'tickets', 'ticketdata', 'messages', 'values', 'channels', 'reports', 'storedreports', 'toplists')]
+        [ValidateSet('sensortree', 'devices', 'sensors', 'tickets', 'ticketdata', 'messages', 'values', 'channels', 'reports', 'storedreports', 'toplists', 'groups')]
         [string]
         $Content,
 
@@ -389,7 +726,7 @@ function Invoke-PRTGCommand
         $Password,
 
         [Parameter()]
-        [string]
+        [string[]]
         $OtherParameters
     )
     Process
@@ -455,7 +792,7 @@ function Invoke-PRTGCommand
 
         if ($OtherParameters)
         {
-            $urlString += $OtherParameters
+            $urlString += $OtherParameters -join '&'
         }
 
         if ($urlString.EndsWith('&'))
@@ -465,6 +802,7 @@ function Invoke-PRTGCommand
 
         try
         {
+            Write-Debug $urlString
             Invoke-WebRequest -Uri $urlString -UseBasicParsing
         }
         catch
